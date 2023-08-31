f4c4db12de0ac22b14b7840f4bb0cde248f8a665
Paul Booker <paul@paulbooker.co.uk>
2023-08-31 19:50:04 +0100

    udemy-37116680-28

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
 });
+
+Route::get('/tasks', function () {
+	return view('index', [
+		'tasks' => \App\Models\Task::latest()->get()
+	]);
+})->name('tasks.index');
+
+Route::get('/tasks/{id}', function ($id) {	
+	return view('show', [
+		'task' => \App\Models\Task::findOrFail($id)
+	]);
+})->name('tasks.show');
