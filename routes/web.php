52456349829e6d7a760432d1d7e678ff750dd91d
Paul Booker <paul@paulbooker.co.uk>
2023-09-06 21:18:48 +0100

    udemy-37116680-42

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use \App\Http\Requests\TaskRequest;
+use \App\Models\Task;
+//use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,12 +23,46 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
 	return view('index', [
-		'tasks' => \App\Models\Task::latest()->get()
+		'tasks' => Task::latest()->paginate(10)
 	]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) {	
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{task}/edit', function (Task $task) {	
+	return view('edit', [
+		'task' => $task
+	]);
+})->name('tasks.edit');
+
+Route::get('/tasks/{task}', function (Task $task) {	
 	return view('show', [
-		'task' => \App\Models\Task::findOrFail($id)
+		'task' => $task
 	]);
 })->name('tasks.show');
+
+Route::post('tasks', function (TaskRequest $request) {
+	$task = Task::create($request->validated());
+	
+	return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task created successfully.');
+	
+})->name('tasks.store');
+
+Route::put('tasks/{task}', function (Task $task, TaskRequest $request) {
+	$task->update($request->validated());	
+	
+	return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully.');
+	
+})->name('tasks.update');
+
+Route::delete('tasks/{task}', function (Task $task)  {
+	$task->delete();
+	
+	return redirect()->route('tasks.index')->with('success', 'Task deleted successfully.');
+})->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+	$task->toggleComplete();
+	
+	return redirect()->back()->with('success', 'Task updated successfully.');
+})->name('tasks.toggle-complete');
